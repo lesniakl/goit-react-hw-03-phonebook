e21a56fea93b870d954d45a4fcd15b6d971bac87
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-03 21:42:16 +0100

    Copy initial files from previous repository

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements.name.value;
+    if (this.state.contacts.some(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const number = form.elements.number.value;
+    const id = nanoid();
+    const newContact = {
+      id,
+      name,
+      number,
+    };
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, newContact] };
+    });
+    form.reset();
+  };
+
+  handleChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  handleDelete = e => {
+    const newContacts = this.state.contacts.filter(
+      contact => contact.name !== e.target.name
+    );
+    this.setState({ contacts: [...newContacts] });
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onAddContact={this.handleSubmit} />
+
+        <h2 className={css.title}>Contacts</h2>
+        <Filter onFilterChange={this.handleChange} filter={this.state.filter} />
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
